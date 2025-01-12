a5cc214e34e4ea20d1637000950bfcfbf287d442
simgeakgul <simgeakgul20@ku.edu.tr>
2025-01-12 21:41:23 +0300

    added book querying

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,7 +1,7 @@
 const db = require('../models/db');
 
 exports.getBooks = async (req, res) => {
-  const { title, author, genre, year } = req.query;
+  const { title, author, genre, year, minPageCount, maxPageCount } = req.query;
   let query = "SELECT * FROM books WHERE 1=1";
   const params = [];
 
@@ -21,10 +21,22 @@ exports.getBooks = async (req, res) => {
     query += " AND year = ?";
     params.push(year);
   }
+  if (minPageCount) {
+    query += " AND pages >= ?";
+    params.push(Number(minPageCount));
+  }
+  if (maxPageCount) {
+    query += " AND pages <= ?";
+    params.push(Number(maxPageCount));
+  }
 
+  console.log("SQL Query:", query);
+  console.log("Query Parameters:", params);
+  
   try {
     const [books] = await db.query(query, params);
     res.status(200).json(books);
+    
   } catch (error) {
     console.error('Database Error:', error);
     res.status(500).json({ error: 'Database error' });
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getBooks } = require('../controllers/bookController');
+const { getBooks, getGenres } = require('../controllers/bookController');
 const router = express.Router();
 
 router.get('/books', getBooks);
